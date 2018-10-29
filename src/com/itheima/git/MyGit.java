4080c51851488ba7275c69aeb753e7d6fd467284
ZhouPeng <383452303@qq.com>
2018-10-29 21:21:37 +0800

    工程

--- src/com/itheima/git/MyGit.java
+++ src/com/itheima/git/MyGit.java
@@ -4,4 +4,9 @@ public class MyGit {
     public static void main(String[] args) {
         System.out.println("我喜欢你，像风吹了八万里，不问归期");
     }
+    public void test(){
+        if(" you is sb".equals("you is sb")){
+            System.out.println("handsome");
+        }
+    }
 }
